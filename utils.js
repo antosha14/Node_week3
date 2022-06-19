4584fa626f4f5ab2f669b0b45f97df21f40a3a0c
antoshastark <antonkozel761@gmail.com>
2022-06-19 16:33:40 +0300

    All done + TS migration. Next ==> bug fixing and message conversion (to json "message": 'Actual message')

--- utils.js
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'fs'
-
-export const userDatabaseUpdater = async function (content) {
-    writeFile('./data/database.json', JSON.stringify(content), (err) => {
-        if (err) return 'Internal server error'
-    })
-}
